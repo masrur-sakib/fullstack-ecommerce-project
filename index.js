fa9e6171db276b9ef714381cb3bd9a7c4ff29b9b
Masrur Sakib <mashrur333@gmail.com>
2020-04-08 02:10:01 +0600

    Orders API Added

--- index.js
+++ index.js
@@ -40,6 +40,26 @@ app.get('/products', (req, res) => {
     });
 })
 
+app.get('/orders', (req, res) => {
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("orders");
+        // perform actions on the collection object
+        // collection.find().limit(2).toArray((err, documents) => {
+        // collection.find({name: 'Mobile'}).toArray((err, documents) => {
+        collection.find().toArray((err, documents) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send({ message: err });
+            }
+            else {
+                res.send(documents);
+            }
+        });
+        client.close();
+    });
+})
+
 app.get('/product/:key', (req,res)=>{
     const key = req.params.key;
 
