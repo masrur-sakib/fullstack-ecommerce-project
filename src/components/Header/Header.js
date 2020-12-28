5ba0d62d749eaebf7b11a021c368b3119e21dde6
Masrur Sakib <mashrur333@gmail.com>
2020-12-28 22:39:40 +0600

    Working on Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,15 +20,44 @@ const Header = () => {
     // console.log(auth.user);
     return (
         <div className="header">
-            <a href="/"><img src={logo} alt=""/></a>
-            {/* <h1>Count: {count} Previous: {previous}</h1>
-            <button onClick={()=>setCount(count-1)}>-</button>
-            <button onClick={()=>setCount(count+1)}>+</button> */}
+            <nav className="navbar navbar-expand-md navbar-light fixed-top site-navbar">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">
+                    <img src={logo} alt="" width="220" height="70"></img>
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="navbar-collapse collapse justify-content-end" id="navbarNavAltMarkup">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <a className="nav-link" aria-current="true" href="#home">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#about">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#skills">Skills</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#projects">Projects</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#blog">Blog</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+            
+            
+            {/* <a href="/"><img src={logo} alt=""/></a>
             <nav>
                 <a href="/shop">Home</a>
                 <a href="/review">Order Review</a>
-                {/* <a href="/shipping">Shipping</a> */}
-                {/* <a href="/login">Sign in</a> */}
                 <a href="/inventory">Inventory</a>
                 {
                     auth.user ? <a href="/login">Sign out</a>
@@ -37,7 +66,7 @@ const Header = () => {
                 {
                     auth.user && <span style={{color: 'orange'}}>{auth.user.name}</span>
                 }
-            </nav>
+            </nav> */}
         </div>
     );
 };
