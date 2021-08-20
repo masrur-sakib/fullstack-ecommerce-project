bdaa94b431a89d945ae8bc97a2594b9df93f8b77
Masrur Sakib <mashrur333@gmail.com>
2021-08-20 15:56:16 +0600

    Not Found Page Bug Fixed.

--- frontend/src/components/NotFound/NotFound.js
+++ frontend/src/components/NotFound/NotFound.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 import "./NotFound.css";
-import notFoundImg from '../../images/not_found.jpg'
 
 const NotFound = () => {
-    return (
-        <div className='NotFound'>
-            <img src={notFoundImg} alt=""/>
-        </div>
-    );
+  return (
+    <div className="not-found-section">
+      <h1 className="not-found-page-title">404</h1>
+      <h4>Page Not Found</h4>
+    </div>
+  );
 };
 
 export default NotFound;
